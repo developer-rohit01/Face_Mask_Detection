1d5e59c69046499206c845d8b7cb52db7af089fd
Rohit <rr5450534@gmail.com>
2026-04-29 23:27:11 +0530

    Update train_model

---
+++ train_model.py
@@ -0,0 +1,47 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import layers, models
+
+data_dir = "dataset/"
+
+datagen = ImageDataGenerator(
+    rescale=1./255,
+    validation_split=0.2
+)
+
+train_data = datagen.flow_from_directory(
+    data_dir,
+    target_size=(224,224),
+    batch_size=4,
+    class_mode='binary',
+    subset='training'
+)
+
+val_data = datagen.flow_from_directory(
+    data_dir,
+    target_size=(224,224),
+    batch_size=4,
+    class_mode='binary',
+    subset='validation'
+)
+
+model = models.Sequential([
+    layers.Conv2D(32, (3,3), activation='relu', input_shape=(224,224,3)),
+    layers.MaxPooling2D(2,2),
+
+    layers.Conv2D(64, (3,3), activation='relu'),
+    layers.MaxPooling2D(2,2),
+
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
+
+model.fit(train_data, validation_data=val_data, epochs=5)
+
+model.save("model.h5")
